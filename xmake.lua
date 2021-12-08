a184fbf9e0b6bcdb6231dccfa12956bda83149bf
Pierre EVEN <evenpierre@orange.fr>
2021-12-08 17:27:03 +0100

    added xmake support

---
+++ xmake.lua
@@ -0,0 +1,8 @@
+add_rules("mode.debug", "mode.release")
+add_rules("plugin.vsxmake.autoupdate")
+
+target(cpputils)
+	set_kind("static")
+	add_headerfiles("include/**.hpp")
+	add_includedirs("include")
+	add_files("src/**.cpp")
